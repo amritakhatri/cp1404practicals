0f0b51109f1294a65f40220eef7b6f507bc87b39
Amrita Khatri <amrita.khatri@my.jcu.edu.au>
2024-03-21 20:24:29 +0800

    final code

---
+++ prac_06/programming_language.py
@@ -0,0 +1,18 @@
+
+class ProgrammingLanguage:
+    """Represent a Programming Language object."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Check if the programming language is dynamically typed."""
+        return self.typing == "Dynamic"
+
+    def __str__(self):
+        """Return a string representation of the ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
