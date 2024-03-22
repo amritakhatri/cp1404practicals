c2189d3a6e7891cae1a670611ce2a01f641ead99
Amrita Khatri <amrita.khatri@my.jcu.edu.au>
2024-03-22 17:49:45 +0800

    final code

---
+++ prac_06/used_cars.py
@@ -0,0 +1,18 @@
+
+from car import Car
+
+# Create a new Car object called "limo" that is initialised with 100 units of fuel.
+limo = Car(100)
+
+# Add 20 more units of fuel to this new car object using the add method.
+limo.add_fuel(20)
+
+# Print the amount of fuel in the car.
+print(f"The amount of fuel in the car: {limo.fuel}")
+
+# Attempt to drive the car 115 km using the drive method.
+distance_driven = limo.drive(115)
+print(f"The car drove {distance_driven} km")
+
+# Print the car object to ensure that the __str__ method is working as expected.
+print(limo)
